97b1d2b8244e03b57b98ed0e1540366340f5cd81
Priti Dhanawade <pritidhanawade12@gmail.com>
2019-11-13 16:08:01 +0530

    conflict

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var emp = require ("./emp");
 app.use("/admin",admin);
 app.use("/employee",emp);
 
-console.log("hello vinit !!!");
+console.log("hello priti  dhanawade vinit !!!");
 
 console.log("hello vinit !!!");
 
@@ -13,6 +13,7 @@ app.listen(8000,function()
 {
     console.log("server started");
 })
+
 
 
 app.listen(8000,function()
