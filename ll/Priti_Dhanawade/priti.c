d0e40e2655e4f6803532d0d30390620ccefb35f8
Priti Dhanawade <pritidhanawade12@gmail.com>
2019-11-13 20:14:02 +0530

    successful

---
+++ ll/Priti_Dhanawade/priti.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+
+hello
+prii
+mahahsk
+jkedhwek
+
+int x=8;
+printf("%d%d%d%d%d",--x,--x,--x,++x,x++);
+return 0;
+
+djkwagfiuwrkhb
+wjdfh
+dsjf
+fjnukhui
+
+sanket 
+padgal
+
+
+}
